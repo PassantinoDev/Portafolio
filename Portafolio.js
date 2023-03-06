e9bab0cfd484ef3509e2a2b662caa303bab673c6
Daniel Passantino <118680085+PassantinoDev@users.noreply.github.com>
2023-03-06 00:50:07 -0400

    Add files via upload

---
+++ Portafolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Frontend Developer", "Youtuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed:300,
+    backDelay:100,
+    loop:true
+})
